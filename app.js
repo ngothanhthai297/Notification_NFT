c50f624232aff468fc26adbaad01440ed2ea56ab
ngo thanh thai <ngothai653@gmail.com>
2022-02-25 15:47:06 +0700

    update

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const router = require('./routers/index')
 
-app.set("view engine","ejs")
-app.set("views","views")
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(require('cors')());
--- controllers/Notification.js
+++ controllers/Notification.js
@@ -9,22 +9,29 @@ exports.index = async (req, res, next) => {
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    let perPage = req.query.total || 5
-    let page = req.query.page || 1
-    Notification_Model.find({ address: req.body.address }, ['address', 'title', 'content', 'images'])
-        .skip((perPage * page) - perPage)
-        .limit(perPage)
-        .exec((err, notifications) => {
-            Notification_Model.countDocuments((err, count) => {
-                if (err) return next(err)
-                // res.send(notifications)
-                res.status(200).json({
-                    status: true,
-                    message: 'Query Success.',
-                    data: notifications
-                });
+    try {
+        let perPage = req.query.total || 5
+        let page = req.query.page || 1
+        Notification_Model.find({ address: req.body.address }, ['address', 'title', 'content', 'images', 'type'])
+            .skip((perPage * page) - perPage)
+            .limit(perPage)
+            .exec((err, notifications) => {
+                Notification_Model.countDocuments((err, count) => {
+                    if (err) return next(err)
+                    res.status(200).json({
+                        status: true,
+                        message: 'Query Success.',
+                        data: notifications
+                    });
+                })
             })
-        })
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            errors: error
+        });
+    }
+   
 }
 
 exports.update = async (req, res) => {
@@ -32,10 +39,10 @@ exports.update = async (req, res) => {
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    if (req.query.id && !req.body.address) {
+    if (req.body.id && !req.body.address) {
         try {
-            const checkExistId = await Notification_Model.find({ _id: req.query.id })
-            Notification_Model.findOneAndUpdate({ id: req.query.id }, { $set: { status: req.body.status } }, (err, msg) => {
+            const checkExistId = await Notification_Model.find({ _id: req.body.id })
+            Notification_Model.findOneAndUpdate({ id: req.body.id }, { $set: { status: req.body.status } }, (err, msg) => {
                 if (err) {
                     res.status(500).json({
                         status: false,
@@ -44,7 +51,7 @@ exports.update = async (req, res) => {
                 } else {
                     res.status(200).json({
                         status: true,
-                        msg: "Update Status Success",
+                        message: "Update Status Success",
                     })
                 }
             })
@@ -55,32 +62,40 @@ exports.update = async (req, res) => {
             });
         }
        
-    } else if (req.body.address && !req.query.id) {
-        const checkExistAddress = await Notification_Model.find({ address: req.body.address })
-        if (checkExistAddress != 0) {
-            Notification_Model.updateMany({ address: req.body.address }, { $set: { status: req.body.status } }, (err, msg) => {
-                if (err) {
-                    res.status(500).json({
-                        status: false,
-                        message: 'Server error. Please try again.'
-                    });
-                } else {
-                    res.status(200).json({
-                        status: true,
-                        msg: "Update Status Success",
-                    })
-                }
-            })
-        } else {
-            res.status(401).json({
+    } else if (req.body.address && !req.body.id) {
+        try {
+            const checkExistAddress = await Notification_Model.find({ address: req.body.address })
+            if (checkExistAddress != 0) {
+                Notification_Model.updateMany({ address: req.body.address }, { $set: { status: req.body.status } }, (err, msg) => {
+                    if (err) {
+                        res.status(500).json({
+                            status: false,
+                            message: 'Server error. Please try again.'
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: true,
+                            message: "Update Status Success",
+                        })
+                    }
+                })
+            } else {
+                res.status(401).json({
+                    status: false,
+                    message: "Address Wrong",
+                })
+            }
+        } catch (error) {
+            res.status(400).json({
                 status: false,
-                msg: "Address Wrong",
+                errors: error,
             })
         }
+       
     } else {
-        res.status(200).json({
+        res.status(400).json({
             status: false,
-            msg: "Please Choose Update By Id Or Address",
+            message: "Please Choose Update By Id Or Address",
         })
     }
     // Notification_Model.findByIdAndUpdate(req.body.id, { status: req.body.status }, (err,notification) =>{
@@ -104,18 +119,23 @@ exports.create = async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
     const { address, title, content, type, images } = req.body
-    const notification = await Notification_Model({
-        address,
-        title,
-        content,
-        type,
-        images
-    })
-    await notification.save()
-
-    res.json({
-        status: 201,
-        notification: notification,
-    })
-
+    try {
+        const notification = await Notification_Model({
+            address,
+            title,
+            content,
+            type,
+            images
+        })
+        await notification.save()
+        res.json({
+            status: 201,
+            notification: notification,
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            errors: error,
+        })
+    }
 }
